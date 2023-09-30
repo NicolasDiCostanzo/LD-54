4eb17283e16bbf4c2c451c78b90606693973deae
Robin Cartier <robin.cartier@protonmail.com>
2023-09-30 18:09:42 +0200

    compass and needle asset

--- Assets/CompassManager.cs
+++ Assets/CompassManager.cs
@@ -3,24 +3,26 @@ using UnityEngine;
 public class CompassManager : MonoBehaviour
 {
     Vector3 exit;
+    Transform player;
     // Start is called before the first frame update
     void Start()
     {
         exit = GameObject.Find("Exit").transform.position;
+        player = GameObject.Find("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 player = GameObject.Find("Player").transform.position;
-
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, GetNeedleRotation(player)));
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, GetNeedleRotation(player.position)));
     }
 
     float GetNeedleRotation(Vector3 playerPostion) {
         Vector3 delta = exit - playerPostion;
         delta.y = 0;
 
-        return - Vector3.SignedAngle(delta, Vector3.forward, Vector3.up);
+        // TODO 
+        return Vector3.SignedAngle(exit, playerPostion, Vector3.up);
+        // return - Vector3.SignedAngle(delta, Vector3.forward, Vector3.up);
     }
 }
